ea666ba47fb4feaa02b415b8cb102edbf7b15ebc
Owen Stranathan <owen+github@appfigures.com>
2018-08-01 18:41:05 -0400

    Things

--- bash/install_kubectl_on_container.sh
+++ bash/install_kubectl_on_container.sh
@@ -1,11 +1,9 @@
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-210.0.0-linux-x86_64.tar.gz
-tar -xvf google-cloud-sdk-210.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-sdk-210.0.0-linux-x86_64.tar.gz
 rm google-cloud-sdk-210.0.0-linux-x86_64.tar.gz
-bash google-cloud-sdk/install.sh -q  # q makes it non-interactive
-bash google-cloud-sdk/bin/gcloud components install kubectl -q  # again is in non interactive
+bash google-cloud-sdk/install.sh -q  --additional-components kubectl  # q makes it non-interactive
 mkdir -p /usr/local/bin
-set -x
 install google-cloud-sdk/bin/kubectl /usr/local/bin
-kubectl get nodes
 
-echo "Tada!"
+rm -rf google-cloud-sdk
+
